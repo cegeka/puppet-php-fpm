d97dd7957dc3e3f1cb1a4138114f4b8e0a073fd9
fdammeke <fabian.dammekens@cegeka.be>
2012-11-12 17:12:52 +0100

    php-fpm init files

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,27 +11,37 @@
 # Sample Usage:
 #
 class php-fpm (
-  $ensure         = present,
-  $pidfile        = '/var/run/php-fpm.pid',
-  $error_log      = '/var/log/php-fpm.log',
-  $log_level      = 'debug',
-  $daemonize      = 'yes',
-  $user           = 'apache',
-  $group          = 'apache',
-  $listen_address = '127.0.0.1:9000',
+  $ensure             = present,
+  $pidfile            = '/var/run/php-fpm.pid',
+  $error_log          = '/var/log/php-fpm.log',
+  $log_level          = 'debug',
+  $daemonize_proc     = 'yes',
+  $user               = 'apache',
+  $group              = 'apache',
+  $listen_address     = '127.0.0.1:9000',
 ){
 
   file { '/etc/php/php-fpm.conf':
     ensure  => $ensure,
-    content => template('php-fpm/php-fpm.erb',
-  } 
+    content => template('php-fpm/php-fpm.erb'),
+    require => File['/etc/php'],
+  }
+
+  file { '/etc/php':
+    ensure => directory,
+  }
 
   file { '/etc/init.d/php-fpm':
     ensure  => $ensure,
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
-    content => template('php-fpm/initd.erb',
-  } 
+    content => template('php-fpm/initd.erb'),
+    require => File['/etc/php/php-fpm.conf'],
+  }
 
+  service { 'php-fpm':
+    ensure  => running,
+    require => File['/etc/init.d/php-fpm'],
+  }
 }
